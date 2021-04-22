e42e384da7cfb538d5b2965957e0f326b3aab68a
Hmiya6 <h.miya.work@gmail.com>
2021-04-22 14:07:38 +0900

    Delete unexpectedly added files

--- target/rls/debug/deps/my_vec-1bc05343e50ded99.d
+++
@@ -1,6 +0,0 @@
-/home/popondayu/Coding/practical-introduction-of-rust/my-vec/target/rls/debug/deps/my_vec-1bc05343e50ded99.rmeta: src/main.rs src/toyvec.rs
-
-/home/popondayu/Coding/practical-introduction-of-rust/my-vec/target/rls/debug/deps/my_vec-1bc05343e50ded99.d: src/main.rs src/toyvec.rs
-
-src/main.rs:
-src/toyvec.rs:
--- target/rls/debug/deps/my_vec-afd39ac6e6ff082a.d
+++
@@ -1,6 +0,0 @@
-/home/popondayu/Coding/practical-introduction-of-rust/my-vec/target/rls/debug/deps/my_vec-afd39ac6e6ff082a.rmeta: src/main.rs src/toyvec.rs
-
-/home/popondayu/Coding/practical-introduction-of-rust/my-vec/target/rls/debug/deps/my_vec-afd39ac6e6ff082a.d: src/main.rs src/toyvec.rs
-
-src/main.rs:
-src/toyvec.rs:
